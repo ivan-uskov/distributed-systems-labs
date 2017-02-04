1294fed1e8139fdac85f95a5221b872126a26c88
Ivan Uskov <ivan.uskov@cpslabs.net>
2017-02-04 21:57:16 +0300

    add saving to redis and sleep for 3 seconds

--- notes/backend/backend/Program.fs
+++ notes/backend/backend/Program.fs
@@ -7,6 +7,20 @@ open Suave.Successful
 open Suave.RequestErrors
 open Newtonsoft.Json
 open System
+open ServiceStack.Redis
+
+
+module Config = 
+    [<Literal>]
+    let REDIS_HOST = "127.0.0.1:6379"
+    [<Literal>]
+    let SAVE_NOTE_ROUTE = "/save-note"
+    [<Literal>]
+    let SLEEP_MILLISECONDS = 4000
+
+type Storage() =
+    let redis = new RedisClient(Config.REDIS_HOST)
+    member this.set key value = redis.Set(key, value)
 
 module Notes =
     type Note = { 
@@ -14,26 +28,47 @@ module Notes =
         mutable id : string
     }
 
-    let serialize = JsonConvert.SerializeObject
-    let deserialize n = JsonConvert.DeserializeObject<Note> n
+    let serializeId note =
+        sprintf "{\"id\":\"%s\"}" note.id
+
+    let generateId =
+        Guid.NewGuid().ToString()
+
+    let store note =
+        note.id <- generateId
+        let storage = new Storage()
+        storage.set note.id (UTF8.bytes note.text)
+        note
+
+    let parseFromRequest request = 
+        try
+            let fromJson n = JsonConvert.DeserializeObject<Note> n
+            request.rawForm |> UTF8.toString |> fromJson
+        with
+            _ -> failwith "Invalid post data"
+
+    let validate note =
+        if note.text = null then
+            failwith "Empty note text"
+        note
 
 let saveNote request = 
     try
-        let note = request.rawForm |> UTF8.toString |> Notes.deserialize
-
-        if note.text = null then
-            failwith "Empty note"
-
-        note.id <- Guid.NewGuid().ToString()
-
-        note.id |> sprintf "{\"id\":\"%s\"}" |> OK
+        request |> Notes.parseFromRequest |> Notes.validate |> Notes.store |> Notes.serializeId |> OK
     with
-        | Failure msg -> BAD_REQUEST "Note text not specified"
-        | _ -> BAD_REQUEST "Invalid data"
+        Failure msg -> BAD_REQUEST msg
+
+let sleep milliseconds: WebPart =
+    fun (x : HttpContext) ->
+        async {
+            do! Async.Sleep milliseconds
+            return! OK "end sleep" x
+        }
 
 let app : WebPart =
     choose [
-        POST >=> path "/save-note" >=> request saveNote 
+        POST >=> path Config.SAVE_NOTE_ROUTE >=> (sleep Config.SLEEP_MILLISECONDS) >=> request saveNote
+        NOT_FOUND "Resource not found"
     ]
 
 [<EntryPoint>]
